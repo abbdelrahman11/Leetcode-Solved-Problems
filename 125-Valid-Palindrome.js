028a556a9357df81eb9a21ee7b3610c59005b6fa
Abdelrahman Ahmed <68480582+abbdelrahman11@users.noreply.github.com>
2024-09-19 11:45:10 +0300

    [125] [Time Beats: 85.40%] [Memory Beats: 67.84%] - LeetPush

--- 125-Valid-Palindrome.js
+++ 125-Valid-Palindrome.js
@@ -6,19 +6,14 @@ var isPalindrome = function (s) {
   s = s.toUpperCase();
   s = s.replace(/\W/g, "");
   s = s.replace("_", "");
-  console.log(s);
   let il = 0;
   let ir = s.length - 1;
-  let left = s[il];
-  let right = s[ir];
   while (il < ir) {
-    if (left != right) {
+    if (s[il] != s[ir]) {
       return false;
     }
     il++;
     ir--;
-    left = s[il];
-    right = s[ir];
   }
   return true;
 };
